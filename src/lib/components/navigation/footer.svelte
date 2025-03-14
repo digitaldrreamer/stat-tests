b1b8e2a2377c8096079793213d0449b51dfbfc49
digitaldrreamer <digitaldrreamer@gmail.com>
2025-03-14 04:02:06 +0100

    ✨ feat(components): implemented header component
    Added authentication features, search functionality, and a responsive mobile menu to the `Header` component.  Updated styling and included user profile dropdown.

--- src/lib/components/navigation/footer.svelte
+++ src/lib/components/navigation/footer.svelte
@@ -81,7 +81,7 @@
                             <rect width="20" height="16" x="2" y="4" rx="2"/>
                             <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/>
                         </svg>
-                        <span class="text-neutral-400">support@fashionhub.com</span>
+                        <span class="text-neutral-400">support@haha.ng</span>
                     </li>
                 </ul>
             </div>
