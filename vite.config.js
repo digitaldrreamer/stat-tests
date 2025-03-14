db1d0095d6eef171dd2f7dbf1f1be1b78b1024d8
digitaldrreamer <digitaldrreamer@gmail.com>
2025-03-14 04:02:46 +0100

    ✨ feat(config): added `allowedHosts` to `vite.config.js`

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		allowedHosts: ['localhost', 'd4aa-102-89-85-136.ngrok-free.app'],
+	}
 });
